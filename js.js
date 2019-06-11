eebb19b271f225d6f7640280364d51c173c3b386
yellowwured <51583385+yellowwured@users.noreply.github.com>
2019-06-11 20:59:05 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -22,6 +22,12 @@ document.getElementById('switchlight').addEventListener('click', function () {  
     document.getElementById("switchlight-div").classList.toggle('switchlight');
 });
 
+function myFunction() {   /**替換文字to translate*/
+    var str = document.getElementById("transtext").innerHTML; 
+    var trn = str.replace(str, "潘孟鉉分別於2001年和2003年獲得台灣國立中正大學和國立清華大學的學士和碩士學位，並獲得博士學位。 他於2008年獲得台灣國立交通大學學位。獲得博士學位後，潘博士在台灣新竹HTMM公司工作了三年，致力於設計和實施3GPP R99 / R5 / R6 第2/3層協議棧。 潘博士現任台灣淡江大學計算機科學與信息工程系副教授。 他還擔任國際分佈式傳感器網絡期刊（IJDSN）的副主編。 他的研究興趣包括無線傳感器網絡，移動計算和LTE-A網絡。 潘博士現在是IEEE的成員。");
+    document.getElementById("afttrans").innerHTML ="<br><br>" +trn;
+  }
+
 function hidemytext(index){
     switch(index){
         case 1:
